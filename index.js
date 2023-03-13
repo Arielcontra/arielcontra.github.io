88eceaa330a3931dfe24f057c9b12b59f14de0b2
Arielcontra <127198997+Arielcontra@users.noreply.github.com>
2023-03-13 18:38:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,62 @@
+const lenguajesArray = [
+    {
+        name: "Java",
+        year: "1995",
+        creators: ["James Gosling"],
+        compiled: true
+    },
+    {
+        name: "Python",
+        year: "1991",
+        creators: ["Guido van Rossum"],
+        compiled: false
+    },
+    {
+        name: "C++",
+        year: "1983",
+        creators: ["Bjarne Stroustrup"],
+        compiled: true
+    },
+    {
+        name: "JavaScript",
+        year: "1995",
+        creators: ["Brendan Eich"],
+        compiled: false
+    },
+    {
+        name: "PHP",
+        year: "1994",
+        creators: ["Rasmus Lerdorf"],
+        compiled: false
+    },
+    {
+        name: "Ruby",
+        year: "1995",
+        creators: ["Yukihiro Matsumoto"],
+        compiled: false
+    },
+    {
+        name: "Swift",
+        year: "2014",
+        creators: ["Chris Lattner"],
+        compiled: true
+    },
+    {
+        name: "Kotlin",
+        year: "2011",
+        creators: ["JetBrains"],
+        compiled: true
+    },
+    {
+        name: "C#",
+        year: "2000",
+        creators: ["Anders Hejlsberg"],
+        compiled: true
+    },
+    {
+        name: "Go",
+        year: "2007",
+        creators: ["Robert Griesemer", "Rob Pike", "Ken Thompson"],
+        compiled: true
+    },
+];
