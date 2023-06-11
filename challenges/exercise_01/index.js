852193008d55a66d56d40a73f0f8c8699eee297a
Arielcontra <127198997+Arielcontra@users.noreply.github.com>
2023-06-11 02:13:52 -0300

    Update index.js

--- challenges/exercise_01/index.js
+++ challenges/exercise_01/index.js
@@ -3,48 +3,56 @@ const btn2 = document.getElementById("2");
 const btn3 = document.getElementById("3");
 const btn4 = document.getElementById("4");
 const btn5 = document.getElementById("5");
+const index = document.getElementById('index');
+const success = document.getElementById('success');
+const select = document.getElementById("select");
 const btnSubmit = document.getElementById("submit");
 const btnSelected = document.getElementsByClassName("button");
-const select = document.getElementById("select");
-const div1 = document.getElementById('div1');
-const div2 = document.getElementById('div2');
+
 
 
 function buttonSelected() { 
   for (let i = 0; i < btnSelected.length; i++) {
     btnSelected[i].classList.remove("button-selected");
-  };
-  this.classList.add("button-selected");
+    };
+    this.classList.add("button-selected");
 };
+
 
 for (let i = 0; i < btnSelected.length; i++) {
     btnSelected[i].addEventListener("click", buttonSelected);
 };
 
+
 function cambiarDiv() {
-    div1.style.display = 'none';
-    div2.style.display = 'flex';
+    index.style.display = 'none';
+    success.style.display = 'flex';
 };
 
 
+function sendPrint(print) {
+    btnSubmit.addEventListener("click", cambiarDiv);
+    select.innerHTML = print;
+  };
+
+  
 btn1.addEventListener("click", function() {
-    select.innerHTML = " 1 ";
+    sendPrint("1");
 });
 
 btn2.addEventListener("click", function() {
-    select.innerHTML = " 2 ";
+    sendPrint("2");
 });
 
 btn3.addEventListener("click", function() {
-    select.innerHTML = " 3 ";
+    sendPrint("3");
 });
 
 btn4.addEventListener("click", function() {
-    select.innerHTML = " 4 ";
+    sendPrint("4");
 });
 
 btn5.addEventListener("click", function() {
-    select.innerHTML = " 5 ";
+    sendPrint("5");
 });
-              
-btnSubmit.addEventListener("click", cambiarDiv);
+
